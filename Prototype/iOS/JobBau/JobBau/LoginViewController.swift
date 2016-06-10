ee4bc862a0f3989ccb5a9107e1b35f0da5f3e238
juliand665 <julian.dunskus@gmail.com>
2016-06-10 17:38:35 +0200

    Prototype v0.0
    It runs, but it doesn’t really do much yet.

---
+++ Prototype/iOS/JobBau/JobBau/LoginViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  JobBau
+//
+//  Created by Julian Dunskus on 10/06/16.
+//  Copyright © 2016 Julian Dunskus. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController, UITextFieldDelegate {
+	
+	@IBOutlet weak var usernameField: UITextField!
+	@IBOutlet weak var passwordField: UITextField!
+	
+	@IBAction func loginPressed(button: UIButton?) {
+		let username = usernameField.text
+		let password = passwordField.text
+		// TODO process these
+		if let editor = storyboard?.instantiateViewControllerWithIdentifier("Editor") {
+			presentViewController(editor, animated: true, completion: nil)
+		}
+	}
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+	func textFieldShouldReturn(textField: UITextField) -> Bool {
+		if textField == usernameField {
+			usernameField.resignFirstResponder()
+			passwordField.becomeFirstResponder()
+		} else {
+			passwordField.resignFirstResponder()
+			loginPressed(nil)
+		}
+		return false
+	}
+	
+}
+
